be756251f7e74860da8ae9794d1aa6106e331f6a
Vadim Gareev <standArtknight@gmail.com>
2019-09-23 20:05:10 +0500

    still

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 ymaps.ready(init);
 var myMap, coords, BalloonLayout;
-var placemarksCollection;
+var placemarksCollection, objectManager;
 
 var placemarkID = 0;
 
@@ -11,12 +11,39 @@ function init() {
         controls: ['zoomControl']
     });
 
-    placemarksCollection = new ymaps.GeoObjectCollection();
+
+    var clustererContentLayout = ymaps.templateLayoutFactory.createClass(
+        // Флаг "raw" означает, что данные вставляют "как есть" без экранирования html.
+        '<h2 class="clusterer_header"><b>{{ properties.place }}</b></h2>' +
+        '<div class="clusterer_body"><a href="#">{{ properties.address }}</a><br><p>{{ properties.comment }}</p></div>' +
+        '<div class="clusterer_footer">{{ properties.date }}</div>'
+    );
+
+    objectManager = new ymaps.ObjectManager({
+        // Чтобы метки начали кластеризоваться, выставляем опцию.
+        clusterize: true,
+        // ObjectManager принимает те же опции, что и кластеризатор.
+        gridSize: 32,
+        geoObjectPreset: 'islands#yellowIcon',
+        geoObjectOpenBalloonOnClick: false,
+        clusterBalloonContentLayout: 'cluster#balloonCarousel',
+        clusterBalloonItemContentLayout: clustererContentLayout,
+        clusterDisableClickZoom: true,
+        clusterOpenBalloonOnClick: true,
+        // Балун никогда не будет открываться в режиме панели.
+        clusterBalloonPanelMaxMapArea: 0,
+        // Устанавливаем размеры макета контента балуна (в пикселях).
+        // clusterBalloonContentLayoutWidth: 200,
+        // clusterBalloonContentLayoutHeight: 130
+    });
+
+    myMap.geoObjects.add(objectManager);
+
 
     mainBalloonLayout = ymaps.templateLayoutFactory.createClass(
         '<div class="my-balloon">' +
         '<div id="header">{{address}}<button id="btn-close">Закрыть</button></div>' +
-        '<div id="comments">Коменты</div>' +
+        '<div id="comments">{{comments | raw}}</div>' +
         '<h2 class="form-name">Ваш отзыв</h2>' +
         '<input type="text" id="user-name" placeholder="Ваше имя">' +
         '<input type="text" id="place" placeholder="Укажите место">' +
@@ -25,107 +52,92 @@ function init() {
         '</div>'
     );
 
-    var clustererContentLayout = ymaps.templateLayoutFactory.createClass(
-        // Флаг "raw" означает, что данные вставляют "как есть" без экранирования html.
-        '<h2 class=ballon_header>{{ properties.place|raw }}</h2>' +
-        '<div class=ballon_body>{{ properties.address|raw }}</div>' +
-        '<div class=ballon_footer>{{ properties.comment|raw }}</div>'
-    );
-    var clusterer = new ymaps.Clusterer({
-        clusterDisableClickZoom: true,
-        clusterOpenBalloonOnClick: true,
-        // Устанавливаем стандартный макет балуна кластера "Карусель".
-        clusterBalloonContentLayout: 'cluster#balloonCarousel',
-        // Устанавливаем собственный макет.
-        clusterBalloonItemContentLayout: clustererContentLayout,
-        // Устанавливаем режим открытия балуна. 
-        // В данном примере балун никогда не будет открываться в режиме панели.
-        clusterBalloonPanelMaxMapArea: 0,
-        // Устанавливаем размеры макета контента балуна (в пикселях).
-        clusterBalloonContentLayoutWidth: 200,
-        clusterBalloonContentLayoutHeight: 130,
-        // Устанавливаем максимальное количество элементов в нижней панели на одной странице
-        clusterBalloonPagerSize: 5
-        // Настройка внешнего вида нижней панели.
-        // Режим marker рекомендуется использовать с небольшим количеством элементов.
-        // clusterBalloonPagerType: 'marker',
-        // Можно отключить зацикливание списка при навигации при помощи боковых стрелок.
-        // clusterBalloonCycling: false,
-        // Можно отключить отображение меню навигации.
-        // clusterBalloonPagerVisible: false
-    });
-
-
-
     myMap.events.add('click', function (e) {
-        console.log(e.get('target'));
+        coords = e.get('coords');
         if (!myMap.balloon.isOpen()) {
-            coords = e.get('coords');
-
             createMainBallon(coords);
-
         } else {
             myMap.balloon.close();
         }
     });
 
-    myMap.events.add('balloonopen', function (e) {
-        let closeButton = document.getElementById('btn-close');
-        let addButton = document.getElementById('btn-add');
+    myMap.balloon.events.add('open', function (e) {
+        const balloonContainer = document.querySelector('.my-balloon');
+        if (balloonContainer !== null) {
+            const closeButton = balloonContainer.querySelector('#btn-close');
+            const addButton = balloonContainer.querySelector('#btn-add');
+            const userName = document.getElementById('user-name');
+            const place = balloonContainer.querySelector('#place');
+            const userComment = balloonContainer.querySelector('#user-comment');
+            const comments = balloonContainer.querySelector('#comments');
+            let coordinates = myMap.balloon.getPosition();
+            const myDate = new Date().toLocaleString();
 
-        let closeListener = ymaps.domEvent.manager.group(closeButton)
-            .add('click', function (event) {
-                myMap.balloon.close();
-            });
+            ymaps.domEvent.manager.group(closeButton)
+                .add('click', function () {
+                    myMap.balloon.close();
+                });
+            ymaps.domEvent.manager.group(addButton)
+                .add('click', function () {
+                    objectManager.add({
+                        type: 'Feature',
+                        id: placemarkID++,
+                        geometry: {
+                            type: 'Point',
+                            coordinates: coordinates
+                        },
+                        properties: {
+                            name: userName.value,
+                            address: myMap.balloon.getData().address,
+                            place: place.value,
+                            comment: userComment.value,
+                            date: myDate
+                        },
+                        options: {
+                            preset: "islands#redDotIcon"
+                        }
+                    });
 
-        let addListener = ymaps.domEvent.manager.group(addButton)
-            .add('click', function (event) {
+                    comments.innerHTML += `<p><b>${userName.value}</b> ${place.value} ${myDate}<br>${userComment.value}</p>`;
 
-                var placemark = new ymaps.Placemark(myMap.balloon.getPosition(), {
-                    place: '<b>МЕСТО</b>',
-                    address: '<b>Адрес</b>',
-                    comment: placemarkID++
-                }, {
-                    openBalloonOnClick: false,
-                    preset: 'islands#icon',
-                    iconColor: '#0095b6'
-                })
-
-                placemarksCollection.add(placemark);
-                clusterer.add(placemark);
-                myMap.geoObjects.add(placemarksCollection).add(clusterer);
-
-
-                // myMap.geoObjects.add(new ymaps.Placemark(myMap.balloon.getData().coords, {
-                //     openBalloonOnClick: false
-                // }, {
-                //     preset: 'islands#icon',
-                //     iconColor: '#0095b6'
-                // }))
-            });
-
+                    userName.value = '';
+                    place.value = '';
+                    userComment.value = '';
+                });
+        }
     });
-    myMap.events.add('balloonclose', function (e) {
-        console.log('hi');
+
+    objectManager.objects.events.add('click', (e) => {
+        var objectId = e.get('objectId');
+        var mark = objectManager.objects.getById(objectId);
+        var commentsHtml = '';
+        var marksArray = objectManager.objects.getAll();
+
+        marksArray
+            .filter(element => element.geometry.coordinates == mark.geometry.coordinates)
+            .forEach(element => {
+                commentsHtml += `<p><b>${element.properties.name}</b> ${element.properties.place} ${element.properties.date}<br>${element.properties.comment}</p>`;
+        });
+
+        if (myMap.balloon.isOpen()) {
+            myMap.balloon.close();
+        }
+
+        createMainBallon(mark.geometry.coordinates, commentsHtml);
     });
 
-    placemarksCollection.events.add('click', function (e) {
-        //e.preventDefault();
+    
 
-
-    });
-
-
-    function createMainBallon(coords, address, commentsHtml) {
-
+    function createMainBallon(coords, commentsHtml) {
+        commentsHtml = commentsHtml || '';
         ymaps.geocode(coords).then(function (res) {
             var firstGeoObject = res.geoObjects.get(0);
-            var myAddress = firstGeoObject.getAddressLine();
 
             myMap.balloon.open(coords, {
-                name: 'HELLO',
+                comments: commentsHtml,
                 coords: coords,
-                address: myAddress
+                address: firstGeoObject.getAddressLine(),
+
             }, {
                 layout: mainBalloonLayout
             });
